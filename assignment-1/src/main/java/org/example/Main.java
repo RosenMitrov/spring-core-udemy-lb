792582676db3ac783017aa2dff8f60e7b10f1d4d
RosenMitrov <rdatabase.m@gmail.com>
2025-04-07 19:41:08 +0300

    Initial commit of assignment 1 without refactoring.

---
+++ assignment-1/src/main/java/org/example/Main.java
@@ -0,0 +1,76 @@
+package org.example;
+
+import org.example.util.CsvForexCalc;
+import org.example.util.InMemoryForexCalc;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.function.Supplier;
+
+public class Main {
+
+    // TODO: Extract as a managed bean?
+    private static Supplier<String> baseCurrncySupplier = () -> "EUR";
+
+    public static void main(String[] args) {
+
+        String sourceCurrency = "EUR";
+        String targetCurrency = "USD";
+        BigDecimal amount = BigDecimal.ONE;
+
+        // TODO: This code is very ugly. We will need a service here, won't we?
+        String baseCurrency = baseCurrncySupplier.get();
+
+        CsvForexCalc csvForexCalc = new CsvForexCalc();
+        InMemoryForexCalc inMemoryForexCalc = new InMemoryForexCalc();
+        // TODO: What about unlimited amount of exrate calculators/repositories?
+        BigDecimal result =  null;
+
+        if (!csvForexCalc.isSupported(sourceCurrency, targetCurrency) &&
+                !inMemoryForexCalc.isSupported(sourceCurrency, targetCurrency)) {
+
+            BigDecimal srcToBase = null, dstToBase = null;
+
+            if (csvForexCalc.isSupported(sourceCurrency, baseCurrency)) {
+                srcToBase = csvForexCalc.convert(sourceCurrency, amount, baseCurrency);
+            } else if (inMemoryForexCalc.isSupported(sourceCurrency, baseCurrency)) {
+                srcToBase = inMemoryForexCalc.convert(sourceCurrency, amount, baseCurrency);
+            }
+
+            if (srcToBase == null) {
+                System.out.println("Cannot convert source to base...");
+                return;
+            }
+
+            //
+            if (csvForexCalc.isSupported(targetCurrency, baseCurrency)) {
+                dstToBase = csvForexCalc.convert(targetCurrency, amount, baseCurrency);
+            } else if (inMemoryForexCalc.isSupported(targetCurrency, baseCurrency)) {
+                dstToBase = inMemoryForexCalc.convert(targetCurrency, amount, baseCurrency);
+            }
+
+            if (dstToBase == null) {
+                System.out.println("Cannot convert target to base...");
+                return;
+            }
+
+            // src/base=X, target/base=Y
+            // src/taget=X/Y
+
+            result = srcToBase.divide(dstToBase, RoundingMode.CEILING).multiply(amount);
+
+
+        } else if (csvForexCalc.isSupported(sourceCurrency, targetCurrency)) {
+            result = csvForexCalc.convert(sourceCurrency, amount, targetCurrency);
+        } else if (inMemoryForexCalc.isSupported(sourceCurrency, targetCurrency)) {
+            result = inMemoryForexCalc.convert(sourceCurrency, amount, targetCurrency);
+        }
+
+        if (result != null) {
+            System.out.println(amount + " " +
+                    sourceCurrency + " equals to " +
+                    result + " " + targetCurrency);
+        }
+
+    }
+}
